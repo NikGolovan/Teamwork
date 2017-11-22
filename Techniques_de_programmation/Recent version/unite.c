73dd933dac627678515c953ee270d273d2bfec92
ReMi-XCVIII <33807239+ReMi-XCVIII@users.noreply.github.com>
2017-11-22 16:08:49 +0100

    Update unite.c

--- Techniques_de_programmation/Recent version/unite.c
+++ Techniques_de_programmation/Recent version/unite.c
@@ -5,7 +5,6 @@
 
 //#include "alerte.h"
 #include "unite.h"
-#include "menu.c"
 
 
 int genererCodeUnite() {
@@ -311,126 +310,6 @@ void supprimerUnite(Unites **unite, int *iCompteurUnite) {
   printf("Unité avec le code %d a été bien supprimé\n", iCodeDonne);
 }
 
-void declancherAlerte(Alerte **alerte, Unites **unite, int *iCompteurAlerte, int *iCompteurUnite) {
-  int i = 0;
-  int iCodeDonneAlerte = 0;
-  int iCodeDonneUnite = 0;
-  int iTmpPositionAlerte = 0;
-  int iTmpPositionUnite = 0;
-  int iBoucleCompteurAlerte = 0;
-  int iBoucleCompteurUnite = 0;
-  int iChoix = 0;
-  char cUniteEnRepos[TAILLE_BUFFER] = "Disponibilité : EN REPOS";
-
-  printf("Les alertes non traitées :\n");
-  for (i = 0; i < *iCompteurAlerte; i++) {
-    if ((*alerte)[i].iEstTraiteParUnite == 1)
-      continue;
-    printf("\n");
-    printf("Code d'alerte : %d\n", (*alerte)[i].iCode);
-    printf("Type d'alerte : %s", (*alerte)[i].cType);
-    printf("Niveau d'alerte : %s", (*alerte)[i].cNiveau);
-    printf("Lieu : %s", (*alerte)[i].cLieu);
-    printf("Nombre de victimes : %d\n", (*alerte)[i].iNombreVictimes);
-    printf("Description : %s", (*alerte)[i].cDescription);
-    printf("Les unités deployés : %d\n", (*alerte)[i].iCodeUniteQuiTraite);
-    iBoucleCompteurAlerte++;
-  }
-
-  if (iBoucleCompteurAlerte == 0) {
-    printf("Toutes les alertes sont traitées par les unités\n");
-    return;
-  }
-
-  printf("\n\n");
-
-  if (*iCompteurUnite == 0) {
-    printf("Aucune unité n'a pas été enregistrée\n");
-    return;
-  }
-
-  printf("Les unités disponibles :\n");
-    for (i = 0; i < *iCompteurUnite; i++) {
-      if ((*unite)[i].iUniteDisponible == 0)
-        continue;
-      printf("\n");
-      printf("Code d'unité : %d\n", (*unite)[i].iCode);
-      printf("Nom d'unité : %s", (*unite)[i].cNom);
-      printf("Moyen de deplacement : %s", (*unite)[i].cMoyenDeplacement);
-      printf("Disponibilité : %s", (*unite)[i].cNiveauDisponibilite);
-      printf("Statut : %s", (*unite)[i].cStatut);
-      printf("Base : %s", (*unite)[i].cBase);
-      printf("Deployée sur les alertes : %d\n", (*unite)[i].iDeployeeSurAlerte);
-      iBoucleCompteurUnite++;
-  }
-
-  if (iBoucleCompteurUnite == 0) {
-    printf("Toutes les unités sont occuppées.\n");
-    printf("CAS DE FORCE MAJEURE\n");
-    printf("1. Faire intervenir les unités en repos ou en reapprovisionnement -2 pour retourner\n");
-    scanf("%d%*c", &iChoix);
-
-    switch(iChoix) {
-      case 1:
-        for (i = 0; i < *iCompteurUnite; i++) {
-          if (strcmp((*unite)[i].cNiveauDisponibilite, cUniteEnRepos) != 1) {
-            printf("Aucune unités est en repos\n");
-            return;
-          } else {
-            printf("\n");
-            printf("Code d'unité : %d\n", (*unite)[i].iCode);
-            printf("Nom d'unité : %s", (*unite)[i].cNom);
-            printf("Moyen de deplacement : %s", (*unite)[i].cMoyenDeplacement);
-            printf("Disponibilité : %s", (*unite)[i].cNiveauDisponibilite);
-            printf("Statut : %s", (*unite)[i].cStatut);
-            printf("Base : %s", (*unite)[i].cBase);
-            printf("Deployée sur les alertes : %d\n", (*unite)[i].iDeployeeSurAlerte);
-          }
-        }
-        break;
-     case -2:
-       return;
-       break;
-     default:
-       printf("La saisie n'est pas correcte.\n");
-       return;
-       break;
-    }
-  }
-
-  printf("\n\n");
-
-  printf("Donnez le code d'alerte :\n");
-  scanf("%d%*c", &iCodeDonneAlerte);
-
-  while (iTmpPositionAlerte < *iCompteurAlerte && iCodeDonneAlerte != (*alerte)[iTmpPositionAlerte].iCode) {
-    iTmpPositionAlerte++;
-  }
-
-  if (iTmpPositionAlerte == *iCompteurAlerte) {
-    printf("Alerte avec le code %d n'a pas été trouvée.\n", iCodeDonneAlerte);
-    return;
-  }
-
-  printf("Donnez code d'unité à envoyer : \n");
-  scanf("%d%*c", &iCodeDonneUnite);
-
-  while (iTmpPositionUnite < *iCompteurUnite && iCodeDonneUnite != (*unite)[iTmpPositionUnite].iCode) {
-      iTmpPositionUnite++;
-   }
-
-  if (iTmpPositionUnite == *iCompteurUnite) {
-    printf("Unité avec le code %d n'a pas été trouvée.\n", iCodeDonneUnite);
-    return;
-  }
-  (*alerte)[iTmpPositionAlerte].iCodeUniteQuiTraite = iCodeDonneUnite;
-  (*alerte)[iTmpPositionAlerte].iEstTraiteParUnite = 1;
-  (*unite)[iTmpPositionUnite].iCompteurRepos += 1;
-  strcpy((*unite)[iTmpPositionUnite].cNiveauDisponibilite, "ACTIVITE\n");
-  strcpy((*unite)[iTmpPositionUnite].cStatut, "EN ALERTE\n");
-  (*unite)[iTmpPositionUnite].iDeployeeSurAlerte = iCodeDonneAlerte;
-  (*unite)[iTmpPositionUnite].iUniteDisponible = 0;
-}
 
 void mettreUniteEnRepos(Unites **unite, int *iCompteurUnite) {
   int i = 0;
