19372beb47b72badc7bcc6a4654200f80a20c35e
NikGolovan <mykolagolovan@gmail.com>
2017-11-23 15:20:02 +0100

    Update alerte.h

--- Techniques_de_programmation/Recent version/alerte.h
+++ Techniques_de_programmation/Recent version/alerte.h
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 #define TAILLE_INITIALE_DE_MALLOC_ALERTE 3
-#define TAILLE_INITIALE_DE_MALLOC_UNITE 3
 #define TAILLE_BUFFER 150
 #define TAILLE_BUFFER_RETOUR 30
+#define NOM_DE_FICHIER_ALERTES "alertes.txt"
 
 typedef struct {
   int iCode;
@@ -18,6 +18,7 @@ typedef struct {
   int iCodeUniteQuiTraite;
 } Alerte;
 
+
 void ajouterAlerte(Alerte **alerte, int *compteurAlerte);
 void afficherUneAlerte(Alerte **alerte, int *compteurAlerte);
 void afficherToutesAlertes(Alerte **alerte, int *compteurAlerte);
@@ -26,6 +27,8 @@ void supprimerAlerte(Alerte **alerte, int *compteurAlerte);
 char * typeAlerte();
 char * niveauAlerte();
 int genererCodeAlerte(/*Alerte **alerte, int *compteurAlerte*/);
-
+void alertesStatiques(Alerte **alerte, int *iCompteurAlerte);
+void supprimerAlerteApresTraitement(Alerte **alerte, int *iCompteurAlerte);
+void imprimerLesAlertes(Alerte **alerte, int *iCompteurAlerte);
 
 #endif
