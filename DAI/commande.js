01dc51813cb5c8b5f6f04669b8868603988adc76
ReMi-XCVIII <33807239+ReMi-XCVIII@users.noreply.github.com>
2017-12-04 13:19:34 +0100

    Update commande.js

--- DAI/commande.js
+++ DAI/commande.js
@@ -1,5 +1,5 @@
 // console.log(JSON.stringify(articles));
-console.log(JSON.stringify(tarifs));
+// console.log(JSON.stringify(tarifs));
 
 let panier = {
   total: 12.5,
@@ -25,22 +25,42 @@ window.addEventListener("load", function() {
 function fabriqueInterfaceGraphique(articles, tarifs) {
   let dirImages = "./images/";
 
+/*  let poivronPhoto = articles[0].photos[0];
+  let poivronsElt = document.getElementsByClassName('article')[0];
+  let poivronImgStyle = poivronsElt.getElementsByClassName('img')[0].style;
+  poivronImgStyle.backgroundImage = "url('" + dirImages + poivronPhoto.url + "')";
+  poivronImgStyle.backgroundPositionX = -poivronPhoto.x + "px";
+  poivronImgStyle.backgroundPositionY = -poivronPhoto.y + "px";
+  poivronImgStyle.width = poivronPhoto.w + "px";
+  poivronImgStyle.height = poivronPhoto.h + "px";
+
+/*  let fraisePhoto = articles[30].photos[0];
+  let fraisesElt = document.getElementsByClassName('article')[2];
+  let fraiseImgStyle = fraisesElt.getElementsByClassName('img')[0].style;
+  fraiseImgStyle.backgroundImage = "url('" + dirImages + fraisePhoto.url + "')";
+  fraiseImgStyle.backgroundPositionX = -fraisePhoto.x + "px";
+  fraiseImgStyle.backgroundPositionY = -fraisePhoto.y + "px";
+  fraiseImgStyle.width = fraisePhoto.w + "px";
+  fraiseImgStyle.height = fraisePhoto.h + "px";
+
+*/
   for (i = 0; i < 31; i++) {
     let articleElt = document.getElementsByClassName('articles')[0];
     let nouvelArticleElt = articleElt.children[0].cloneNode();
     nouvelArticleElt.innerHTML = articleElt.children[0].innerHTML;
     let nouvelArticlePhoto = articles[i].photos[0];
     let nouvelArticleImgStyle = nouvelArticleElt.getElementsByClassName('img')[0].style;
-    let nouvelArticleName = document.getElementsByClassName('nom')[0].style;
+    let nouvelArticleName = nouvelArticleElt.getElementsByClassName('nom')[0];
+    let nouvelArticleOrigin = nouvelArticleElt.getElementsByClassName('origine')[0];
     nouvelArticleImgStyle.backgroundImage = "url('" + dirImages + nouvelArticlePhoto.url + "')";
     nouvelArticleImgStyle.backgroundPositionX = -nouvelArticlePhoto.x + "px";
     nouvelArticleImgStyle.backgroundPositionY = -nouvelArticlePhoto.y + "px";
     nouvelArticleImgStyle.width = nouvelArticlePhoto.w + "px";
     nouvelArticleImgStyle.height = nouvelArticlePhoto.h + "px";
-    nouvelArticleName = articles[i].nom;
+    nouvelArticleName.innerHTML = articles[i].nom;
+    nouvelArticleOrigin.innerHTML = tarifs[i].origine;
     articleElt.appendChild(nouvelArticleElt);
   }
-}
 }
 
 function envoyerCommande() {
@@ -52,4 +72,12 @@ function envoyerCommande() {
   corps = encodeURIComponent(corps);
   let uri = "mailto:" + email + "?subject=" + sujet + "&body=" + corps;
   window.location.href = uri;
+}
+
+function ajouterElt() {
+  var article = document.getElementsByClassName('article');
+  let nouvelArticle = article.children[0].innerHTML;
+
+
+  article.appendChild(nouvelArticle);
 }
