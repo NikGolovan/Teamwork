d0e3d261e716dfb8d57978ffc3c6c76d76fb2982
NikGolovan <mykolagolovan@gmail.com>
2018-04-23 14:26:40 +0200

    Update main.cpp

--- Projet/main.cpp
+++ Projet/main.cpp
@@ -1,5 +1,28 @@
+#include "poker_holdem.hpp"
+
+using namespace std;
 
 int main(int argc, char const *argv[]) {
-  
+  //TODO Stack<Carte> deck(52);
+  Joueurs **joueurs;
+  int nombre_joueurs;
+  Carte *board = NULL;
+
+  //TODO remplirDeck(deck);
+
+  do {
+
+    cout << "Donnez le nombre de joueurs (entre 2 et 10) : ";
+    cin >> nombre_joueurs;
+
+  } while(nombre_joueurs >= 2 && nombre_joueurs <= 10);
+
+  joueurs = new Joueurs *[nombre_joueurs];
+
+  for (int nbTour = 0; nbTour < 4; nbTour++) {
+    tour(/*TODO deck, */board, joueurs, nombre_joueurs, nbTour);
+  }
+
+
   return 0;
 }
