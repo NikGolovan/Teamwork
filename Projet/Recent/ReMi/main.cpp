f03bd1874a6e6ddcf9fa4de4badad64bf41dfa1a
ReMi-XCVIII <33807239+ReMi-XCVIII@users.noreply.github.com>
2018-05-09 15:50:09 +0200

    Add files via upload

---
+++ Projet/Recent/ReMi/main.cpp
@@ -0,0 +1,20 @@
+#include "poker_holdem.hpp"
+
+int main(int argc, char const *argv[]) {
+  Deck deck[TAILLE_DECK];
+  Joueurs *joueurs;
+  int nombreJoueurs = 2;
+  Plateau plateau[TAILLE_PLATEAU];
+
+  srand(time(NULL));
+
+  remplirDeck(deck);
+
+  joueurs = new Joueurs[nombreJoueurs];
+
+  for (int nbTour = 0; nbTour < 4; nbTour++) {
+    tour(deck, plateau, joueurs, nombreJoueurs, nbTour);
+  }
+
+  return 0;
+}
